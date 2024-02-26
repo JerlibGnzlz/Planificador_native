3b4f625cec2b34801739c33e0259624cc9704e1f
jerlibgnzlz <jerlibgnzlz@gmail.com>
2024-02-26 19:09:11 -0300

    feature el layout

---
+++ src/components/Header.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { Text, View, StyleSheet, SafeAreaView } from 'react-native'
+
+const Header = () => {
+    return (
+        <SafeAreaView style={styles.header}>
+            <Text style={styles.texto}>
+                Planificador de gastos..!
+            </Text>
+        </SafeAreaView>
+    )
+}
+
+
+const styles = StyleSheet.create({
+    header: {
+        backgroundColor: '#3b82f6',
+    },
+    texto: {
+        textAlign: "center",
+        fontSize: 30,
+        color: "#fff",
+        textTransform: "uppercase",
+        fontWeight: "bold",
+        paddingVertical: 20
+
+    }
+});
+
+export default Header
