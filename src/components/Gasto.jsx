5bbf528330e21ac37410e5df3cac13076030c9de
jerlibgnzlz <jerlibgnzlz@gmail.com>
2024-04-17 21:48:16 -0300

    style gasto

--- src/components/Gasto.jsx
+++ src/components/Gasto.jsx
@@ -1,17 +1,35 @@
-import { View, Text, StyleSheet } from "react-native"
+import { View, Text, StyleSheet, Image } from "react-native"
 import globalStyles from "../styles";
 
 
-
+const diccionarioIconos = {
+    ahorro: require("../img/icono_ahorro.png"),
+    comida: require("../img/icono_comida.png"),
+    casa: require("../img/icono_casa.png"),
+    gasto: require("../img/icono_gastos.png"),
+    ocio: require("../img/icono_ocio.png"),
+    salud: require("../img/icono_salud.png"),
+    suscripciones: require("../img/icono_suscripciones.png")
+}
 
 export const Gasto = ({ gasto }) => {
 
     const { nombre, cantidad, categoria } = gasto
+
     return (
         <View style={styles.contenedor}>
+            <View>
+
+                <Image
+                    source={diccionarioIconos[categoria]}
+                />
+
+                <Text> {categoria}</Text>
+                <Text> {nombre}</Text>
+            </View>
             <Text style={styles.titulo} >
-                {nombre}
             </Text>
+            <Text> {cantidad}</Text>
         </View>
     )
 }
